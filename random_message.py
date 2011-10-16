7094ff3f7c5808fc5b7f87fd7f919bc90be63318
kutu <kutu182@gmail.com>
2011-10-16 22:57:30 +0400

    fixes after merge

--- random_message.py
+++ random_message.py
@@ -1,11 +1,11 @@
-import sublime, sublime_plugin, re
+import sublime, sublime_plugin
 from urllib2 import urlopen
 
 class RandomMessageCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		data = urlopen('http://whatthecommit.com/index.tx')
+		data = urlopen('http://whatthecommit.com/index.txt')
 		if not data:
 			return
-		message = data.read()
+		message = data.read().strip()
 		for region in self.view.sel():
 			self.view.replace(edit, region, message)
