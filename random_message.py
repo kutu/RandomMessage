80e9a3dc2bcc1f76492cc4bb4f4b9daa6fb104b6
Grigory Bakunov <bobuk@justos.org>
2011-10-16 21:54:45 +0400

    Update random_message.py

--- random_message.py
+++ random_message.py
@@ -3,12 +3,9 @@ from urllib2 import urlopen
 
 class RandomMessageCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		data = urlopen('http://whatthecommit.com/')
+		data = urlopen('http://whatthecommit.com/index.tx')
 		if not data:
 			return
-		m = re.match(r'.*<p>\s*(.+?)\s*</p>', data.read(), re.S)
-		if not m:
-			return
-		message = m.group(1)
+		message = data.read()
 		for region in self.view.sel():
 			self.view.replace(edit, region, message)
