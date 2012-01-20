9995dc3c9c7f9de1d95064b458d1b5c42ec77465
kutu <kutu182@gmail.com>
2012-01-20 17:15:20 +0400

    threading

--- random_message.py
+++ random_message.py
@@ -1,11 +1,37 @@
 import sublime, sublime_plugin
-from urllib2 import urlopen
+import threading
+import urllib2
 
 class RandomMessageCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		data = urlopen('http://whatthecommit.com/index.txt')
-		if not data:
+		thread = WhatTheCommitCallThread(self.view, edit)
+		thread.start()
+		self.handle_thread(thread)
+
+	def handle_thread(self, thread):
+		if thread.is_alive():
+			sublime.set_timeout(lambda: self.handle_thread(thread), 100)
 			return
-		message = data.read().strip()
-		for region in self.view.sel():
-			self.view.replace(edit, region, message)
+		if thread.result:
+			for region in thread.view.sel():
+				thread.view.replace(thread.edit, region, thread.result)
+		if thread.error:
+			sublime.status_message('%s: %s' % (__name__, thread.error))
+
+class WhatTheCommitCallThread(threading.Thread):
+	def __init__(self, view, edit):
+		self.view = view
+		self.edit = edit
+		self.result = None
+		self.error = None
+		super(WhatTheCommitCallThread, self).__init__()
+
+	def run(self):
+		try:
+			req = urllib2.Request('http://whatthecommit.com/index.txt',
+				headers={'User-Agent': 'Sublime Text 2 - Random Message'})
+			res = urllib2.urlopen(req, timeout=5)
+			self.result = res.read().strip()
+		except Exception, e:
+			self.error = e
+			raise
